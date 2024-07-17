90bb6bc4f15b535400c2e4d33b7f33d290fb7adb
Bruna Ester Rolim <brunaerrolim@gmail.com>
2024-07-17 12:12:44 -0300

    fix: fixing issues

--- app/src/main/java/com/example/reminderapp/ui/theme/uilayer/screen/ReminderList.kt
+++ app/src/main/java/com/example/reminderapp/ui/theme/uilayer/screen/ReminderList.kt
@@ -9,18 +9,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.reminderapp.model.Reminder
+import com.example.reminderapp.ui.theme.uilayer.screen.components.ReminderItem
 import java.text.SimpleDateFormat
 import java.util.Locale
 
 @Composable
 fun ReminderList(reminders: List<Reminder>, onDelete: (Reminder) -> Unit) {
     LazyColumn {
-
         reminders.groupBy { reminder ->
             SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(reminder.date)
         }.forEach { (date, reminders) ->
             item {
-
+                Text(
+                    text = "Novo lembrete",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier
+                        .padding(0.dp, 0.dp, 0.dp, 8.dp)
+                )
             }
             items(reminders) { reminder ->
                 ReminderItem(reminder = reminder, onDelete = onDelete)
